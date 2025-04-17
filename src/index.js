a38bd6fd19f3963772c3adf1fe2d1bfbd2d97ef3
Israel4u <israeloluwasholao@gmail.com>
2025-04-17 09:07:54 +0100

    Adding images to the site.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
 import MobileMenu from './assets/modules/MobileMenu'
 import './styles/style.scss'
 
+// Images
+import HeroImage from './assets/images/graduate_no_background.png'
+import PersonalImage from './assets/images/graduate_flipped.png' 
+
+const heroImage = document.querySelector('.hero__img-1')
+const personalImage = document.querySelector('.personal__img-1')
+heroImage.src = HeroImage
+personalImage.src = PersonalImage
+
+
 let mobileMenu = new MobileMenu();
 
 if(module.hot) {
