1284f12eb43dabaceecd743baeb814337cd87d97
Israel4u <israeloluwasholao@gmail.com>
2025-04-21 06:34:22 +0100

    Completed the mobile menu navigation.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import generateJoke from "./generate.joke";
 import MobileMenu from './assets/modules/MobileMenu'
 import './styles/style.scss'
 
@@ -57,9 +56,3 @@ let mobileMenu = new MobileMenu();
 if(module.hot) {
   module.hot.accept()
 }
-// import test from './assets/test-2.jpg'
-
-// const testImage = document.getElementById('test-img')
-// testImage.src = test
-
-// console.log(generateJoke())
